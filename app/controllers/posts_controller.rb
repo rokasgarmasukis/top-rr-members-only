9aff18cea2568087d1d26ba7bf90889a9fd2aab6
ROKAS GARMASUKIS <ROKAS.GARMASUKIS@GMAIL.COM>
2023-02-24 18:49:42 +0100

    Implement basic post creation and authentication functionality

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,27 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, only: %i[new create]
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body).merge(user_id: current_user.id)
+  end
+end
---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ApplicationRecord
+  belongs_to :user
+
+  validates :title, length: { minimum: 5, maximum: 20 }
+  validates :body, length: { minimum: 3, maximum: 100 }
+end
